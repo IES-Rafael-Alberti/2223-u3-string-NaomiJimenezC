66fed666cdd00391cc36c4f2e110487333d19292
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-10-30 17:23:53 +0100

    ejercicio 2 hecho

---
+++ src/ejercicio2.py
@@ -0,0 +1,6 @@
+"""
+Dado que fruta es una variable de tipo cadena, ¿qué significa fruta[:]?
+"""
+
+#Significa obtener toda la cadena almacenada en la variable
+#Si fruta = "Manzana", pues al poner fruta[:] eso equivaldría a "Manzana"
