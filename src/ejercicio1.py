5e8a13e6b1e4b8568c5eaa45199a34bb6572af2e
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-10-30 17:18:03 +0100

    ejercicio 1 hecho

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -10,7 +10,26 @@ def solicitar_palabra()-> str:
             palabra_ingresada = input("Ingrese una palabra: ")
     return palabra_ingresada
 #Procesado
+
+def invertir_palabra(cadena:str) -> str:
+    palabra_invertida = ""
+    contador = 0
+    #debe tener while
+    while contador != len(cadena):
+        palabra_invertida += cadena[-(contador+1)]+ "\n"   
+        contador +=1
+    return palabra_invertida
+
 #Salida
 
+def mostrar_resultado(cadena_invertida:str)->str:
+    return f"Aquí tiene la cadena invertida: {cadena_invertida}"
+
 if __name__ == "__main__":
-    print()
+    cadena_ingresada = solicitar_palabra()
+    
+    cadena_invertida = invertir_palabra(cadena_ingresada)
+    
+    mensaje_de_la_cadena_invertida = mostrar_resultado(cadena_invertida)
+    
+    print(mensaje_de_la_cadena_invertida)
--- test/test_ejercicio1.py
+++ test/test_ejercicio1.py
@@ -1,4 +1,10 @@
-from src.ejercicio1 import*
+from src.ejercicio1 import invertir_palabra,mostrar_resultado
 
 def test_invertir_palabra():
     assert invertir_palabra("hola") == "a\nl\no\nh\n"
+
+def test_invertir_cadena():
+    assert invertir_palabra(" ")== " \n"
+    
+def test_mostrar_cadena_invertida():
+    assert mostrar_resultado("a\nl\no\nh\n") == "Aquí tiene la cadena invertida: \n a\nl\no\nh\n"
